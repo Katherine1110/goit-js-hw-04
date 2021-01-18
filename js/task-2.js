3d32ebf72fd490b730625d806e208d9a80dc9a46
Katherine1110 <53606504+Katherine1110@users.noreply.github.com>
2021-01-18 18:24:26 +0200

    upd task-2

--- js/task-2.js
+++ js/task-2.js
@@ -1,32 +1,66 @@
-// Исправь ошибки которые будут в консоли, чтобы скрипт заработал.
+// // Исправь ошибки которые будут в консоли, чтобы скрипт заработал.
 
 const inventory = {
-    items: ['Knife', 'Gas mask'],
-    add(itemName) {
-      console.log(`Adding ${itemName} to inventory`);
+  items: ['Knife', 'Gas mask'],
+  add(itemName) {
+    console.log(`Adding ${itemName} to inventory`);
 
-      inventory.items.push(itemName);
-    },
-    remove(itemName) {
-      console.log(`Removing ${itemName} from inventory`);
+    this.items.push(itemName);
+  },
+  remove(itemName) {
+    console.log(`Removing ${itemName} from inventory`);
+
+    this.items = this.items.filter(item => item !== itemName);
+  },
+};
+
+const invokeInventoryAction = function(itemName, action) {
+  console.log(`Invoking action on ${itemName}`);
+  action.call(this, itemName);
+};
+
+invokeInventoryAction.call(inventory, 'Medkit', inventory.add);
+// Invoking action on Medkit
+// Adding Medkit to inventory
+
+console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
+
+invokeInventoryAction.call(inventory, 'Gas mask', inventory.remove);
+// Invoking action on Gas mask
+// Removing Gas mask from inventory
+
+console.log(inventory.items); // ['Knife', 'Medkit']
+
+
+
+
+// const inventory = {
+//     items: ['Knife', 'Gas mask'],
+//     add(itemName) {
+//       console.log(`Adding ${itemName} to inventory`);
+
+//       inventory.items.push(itemName);
+//     },
+//     remove(itemName) {
+//       console.log(`Removing ${itemName} from inventory`);
   
-      inventory.items = inventory.items.filter(item => item !== itemName);
-    },
-  };
+//       inventory.items = inventory.items.filter(item => item !== itemName);
+//     },
+//   };
   
-  const invokeInventoryAction = function(itemName, action) {
-    console.log(`Invoking action on ${itemName}`);
-    action(itemName);
-  };
+//   const invokeInventoryAction = function(itemName, action) {
+//     console.log(`Invoking action on ${itemName}`);
+//     action(itemName);
+//   };
   
-  invokeInventoryAction('Medkit', inventory.add);
-  // Invoking action on Medkit
-  // Adding Medkit to inventory
+//   invokeInventoryAction('Medkit', inventory.add);
+//   // Invoking action on Medkit
+//   // Adding Medkit to inventory
   
-  console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
+//   console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
   
-  invokeInventoryAction('Gas mask', inventory.remove);
-  // Invoking action on Gas mask
-  // Removing Gas mask from inventory
+//   invokeInventoryAction('Gas mask', inventory.remove);
+//   // Invoking action on Gas mask
+//   // Removing Gas mask from inventory
   
-  console.log(inventory.items); // ['Knife', 'Medkit']
+//   console.log(inventory.items); // ['Knife', 'Medkit']
